7856b37bcb12004646c996b0848093b953ffc243
jealous-23 <jgrecide.school@gmail.com>
2026-01-05 22:37:40 +0800

    Update Main.java

--- src/finalproject/classes/Main.java
+++ src/finalproject/classes/Main.java
@@ -7,42 +7,7 @@ import finalproject.ui.TilePacerFrame;
 
 public class Main {
 	public static void main(String[] args) {
-		var record = new GameManager();
-		
-		/*record.startGame(GameManager.Difficulty.HARD);
-		record.incrementScore(3);
-		record.saveHighScore();
-		
-		record.startGame(GameManager.Difficulty.EASY);
-		record.incrementScore(3);
-		record.saveHighScore();
-		
-		record.startGame(GameManager.Difficulty.HARD);
-		record.incrementScore(4);
-		record.saveHighScore();
-		
-		record.startGame(GameManager.Difficulty.MEDIUM);
-		record.incrementScore(100);
-		record.saveHighScore();
-		
-		record.startGame(GameManager.Difficulty.HARD);
-		record.incrementScore(104);
-		record.saveHighScore();
-		
-		record.startGame(GameManager.Difficulty.EASY);
-		record.incrementScore(6);
-		record.saveHighScore();
-		
-		record.startGame(GameManager.Difficulty.EASY);
-		record.incrementScore(129);
-		record.saveHighScore();
-		
-		System.out.println(record.getHighScore(GameManager.Difficulty.HARD));
-		
-		GameTile tile = new GameTile(GameTile.TileType.NORMAL, "newCrown.png");
-		tile.revealTile();*/
-		
 		SwingUtilities.invokeLater(TilePacerFrame::new);
-		
 	}
 }
+
